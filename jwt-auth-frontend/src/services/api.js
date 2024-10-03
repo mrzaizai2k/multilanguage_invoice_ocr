5cf1ccc530fa46ac3315adcce8882bb5478229a0
mrzaizai2k <chibao24.12.1999@gmail.com>
2024-10-03 21:11:45 +0700

    NEW HOST

--- jwt-auth-frontend/src/services/api.js
+++ jwt-auth-frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = 'http://54.169.56.249/api';
+export const API_URL = 'http://47.129.87.37/api';
 // export const API_URL = 'http://localhost:8149';
 
 console.log("API_URL", API_URL)
