08ac251fa3f20f8fc753da915007218894d3e0c8
mrzaizai2k <chibao24.12.1999@gmail.com>
2025-04-14 10:24:47 +0700

    fix double logs

--- src/Utils/logger.py
+++ src/Utils/logger.py
@@ -7,24 +7,22 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 from src.Utils.utils import read_config
 
-def create_logger(config_path:str = "config.yaml"):
-    config = read_config(path = config_path)['logging']
+def create_logger(config_path: str = "config.yaml"):
+    config = read_config(path=config_path)['logging']
     logfile = config['logfile']
     log_dir = os.path.dirname(logfile)
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
 
-
     when = config['when']
     backupCount = config['backupCount']
-    logger = logging.getLogger()
-    # Set the logging level
+    
+    logger = logging.getLogger()  # Root logger
     logger.setLevel(logging.DEBUG)
-    # Set levels for other libraries
 
-    libraries_config = config.get('libraries', {})
-    for lib, level in libraries_config.items():
-        logging.getLogger(lib).setLevel(getattr(logging, level))
+    # Remove any existing handlers
+    while logger.handlers:
+        logger.removeHandler(logger.handlers[0])
 
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.DEBUG)
@@ -32,10 +30,17 @@ def create_logger(config_path:str = "config.yaml"):
     rotate_handler = TimedRotatingFileHandler(filename=logfile, when=when, backupCount=backupCount)
     rotate_handler.setLevel(logging.DEBUG)
     rotate_handler.suffix = "%Y%m%d"
-        
-    formatter = logging.Formatter('%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d] - Message: %(message)s')
-    rotate_handler.setFormatter(formatter)
+
+    formatter = logging.Formatter('hahahahahahahaha %(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d] - Message: %(message)s')
     console_handler.setFormatter(formatter)
+    rotate_handler.setFormatter(formatter)
+
     logger.addHandler(console_handler)
     logger.addHandler(rotate_handler)
+
+    # Configure levels for other libraries
+    libraries_config = config.get('libraries', {})
+    for lib, level in libraries_config.items():
+        logging.getLogger(lib).setLevel(getattr(logging, level))
+
     return logger
