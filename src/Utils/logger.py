9181d90222a2363de331ed75a43da9587934e38c
mrzaizai2k <chibao24.12.1999@gmail.com>
2025-04-14 10:43:33 +0700

    completely solve double logs

--- src/Utils/logger.py
+++ src/Utils/logger.py
@@ -8,39 +8,28 @@ from logging.handlers import TimedRotatingFileHandler
 from src.Utils.utils import read_config
 
 def create_logger(config_path: str = "config.yaml"):
-    config = read_config(path=config_path)['logging']
-    logfile = config['logfile']
-    log_dir = os.path.dirname(logfile)
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-
-    when = config['when']
-    backupCount = config['backupCount']
     
-    logger = logging.getLogger()  # Root logger
+    config = read_config(path=config_path)['logging']  # Assume this reads your config
+    logfile = config['logfile']
+    os.makedirs(os.path.dirname(logfile), exist_ok=True)
+
+    logger = logging.getLogger('myapp')
     logger.setLevel(logging.DEBUG)
+    logger.propagate = False  # Key fix to stop duplication
 
-    # Remove any existing handlers
-    while logger.handlers:
-        logger.removeHandler(logger.handlers[0])
-
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
-
-    rotate_handler = TimedRotatingFileHandler(filename=logfile, when=when, backupCount=backupCount)
-    rotate_handler.setLevel(logging.DEBUG)
-    rotate_handler.suffix = "%Y%m%d"
-
-    formatter = logging.Formatter('hahahahahahahaha %(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d] - Message: %(message)s')
-    console_handler.setFormatter(formatter)
-    rotate_handler.setFormatter(formatter)
-
-    logger.addHandler(console_handler)
-    logger.addHandler(rotate_handler)
-
-    # Configure levels for other libraries
-    libraries_config = config.get('libraries', {})
-    for lib, level in libraries_config.items():
-        logging.getLogger(lib).setLevel(getattr(logging, level))
+    if not logger.handlers:  # Only add handlers if none exist
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)
+        rotate_handler = TimedRotatingFileHandler(
+            filename=logfile, when=config['when'], backupCount=config['backupCount']
+        )
+        rotate_handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(
+            '%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d] - Message: %(message)s'
+        )
+        console_handler.setFormatter(formatter)
+        rotate_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+        logger.addHandler(rotate_handler)
 
     return logger
